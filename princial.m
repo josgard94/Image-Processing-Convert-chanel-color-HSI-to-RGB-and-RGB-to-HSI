a6dec68fa4c7b8ffc912516cf76d0ae134e21e62
Edgard Diaz <50925001+josgard94@users.noreply.github.com>
2019-06-19 16:05:55 -0500

    Update princial.m

--- princial.m
+++ princial.m
@@ -1,15 +1,20 @@
+%   Autor: Edgard Diaz
+%   contact: e.diaz@nartsoft.com.mx
+%   date: 10 - 06 - 2019
+%   Codigo princial, ejecuta los archivos que reliazan las conversiones de RGB a HSI y de HSI a RGB
+
 clear,clc
 imagen = imread("bridge.tif");
 
- [H,S,I] = ConvertRgbToHsi(im2double(imagen));
- imgHSI = cat(3,H,S,I);
- figure(2)
- imshow(im2uint8(imgHSI));
- figure(1)
- subplot(1,2,1);
- imshow(im2uint8(imagen));
- [R,G,B] = ConvertHsiToRgb(im2double(imgHSI));
- imgRGB = im2uint8(cat(3,R,G,B));
- 
- subplot(1,2,2);
- imshow(imgRGB);
+[H,S,I] = ConvertRgbToHsi(im2double(imagen));
+imgHSI = cat(3,H,S,I);
+figure(2)
+imshow(im2uint8(imgHSI));
+figure(1)
+subplot(1,2,1);
+imshow(im2uint8(imagen));
+[R,G,B] = ConvertHsiToRgb(im2double(imgHSI));
+imgRGB = im2uint8(cat(3,R,G,B));
+
+subplot(1,2,2);
+imshow(imgRGB);
